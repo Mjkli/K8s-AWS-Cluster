96f4c9a0921258e8fa764915cae66bc3574f217c
anthony bruno <anthonybruno63@yahoo.com>
2023-03-22 08:04:50 -0700

    adding ability to retreive ssh key

--- Infra/dev/output.tf
+++ Infra/dev/output.tf
@@ -2,3 +2,8 @@ output "ansible-public-ip" {
   value       = module.ansible-eip.public_ip
   sensitive   = true
 }
+
+output "tls_key" {
+  value = module.ansible_key.public_key
+  sensitive = true
+}
