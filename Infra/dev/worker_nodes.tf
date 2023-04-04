781caaf234f2efcfefcb47fe5d925a822900328b
anthony bruno <anthonybruno63@yahoo.com>
2023-04-03 19:32:13 -0700

    passing no sg for worker nodes and using the same sg for the master node

--- Infra/dev/worker_nodes.tf
+++ Infra/dev/worker_nodes.tf
@@ -8,7 +8,7 @@ module "worker_node_1" {
     env = var.env
     name = "worker-node-2-${var.project}-${var.env}"
     key_name = "${var.project}-${var.env}-${var.key_name}"
-    security_groups = []
+    security_groups = null
 
 }
 
@@ -22,6 +22,6 @@ module "worker_node_2" {
     env = var.env
     name = "worker-node-2-${var.project}-${var.env}"
     key_name = "${var.project}-${var.env}-${var.key_name}"
-    security_groups = []
+    security_groups = null
 
 }
