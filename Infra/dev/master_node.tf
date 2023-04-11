c81f9b72c6d6676ea86fcb30fc0bbdc4755b4783
anthony bruno <anthonybruno63@yahoo.com>
2023-04-11 11:02:03 -0700

    adding master node as a runner and adding nodes back

--- Infra/dev/master_node.tf
+++ Infra/dev/master_node.tf
@@ -1,14 +1,35 @@
-# module "master_node" {
-#     source = "../modules/ec2/instance"
-#     ami = var.master_node_ami
-#     instance_type = var.master_node_instance_type
-#     subnet = module.subnet-1-public.id
-#     private_ip = var.masterNode_ip
-#     project = var.project
-#     env = var.env
-#     user_data = var.worker_user_data
-#     name = "master-node-${var.project}-${var.env}"
-#     key_name = "${var.project}-${var.env}-${var.key_name}"
-#     security_groups = [module.ansible_sg.id]
+module "master_node" {
+    source = "../modules/ec2/instance"
+    ami = var.master_node_ami
+    instance_type = var.master_node_instance_type
+    subnet = module.subnet-1-public.id
+    private_ip = var.masterNode_ip
+    project = var.project
+    env = var.env
+    name = "master-node-${var.project}-${var.env}"
+    key_name = "${var.project}-${var.env}-${var.key_name}"
+    security_groups = [module.ansible_sg.id]
+    user_data = <<-EOL
+    #! /bin/bash
+    apt update
+    apt upgrade -y
+    apt install ansible jq -y
 
-# }
+    cd /home/ubuntu
+    mkdir actions-runner && cd actions-runner
+    curl -o actions-runner-linux-x64-2.303.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.303.0/actions-runner-linux-x64-2.303.0.tar.gz
+    tar xzf ./actions-runner-linux-x64-2.303.0.tar.gz
+
+    chown -R ubuntu /home/ubuntu
+
+
+    echo "$(curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${var.api_token}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/mjkli/iac_full/actions/runners/registration-token | jq -r '.token')" > token.txt
+    echo "export RUNNER_TOKEN=$(cat token.txt)" >> /etc/environment
+    rm token.txt
+    
+    sudo -H -u ubuntu bash -c 'source /etc/environment && ./config.sh --url https://github.com/Mjkli/IAC_full --token $RUNNER_TOKEN --name master-node --unattended --runasservice'
+    sudo -H -u ubuntu bash -c './run.sh &'
+
+    EOL
+
+}
--- Infra/dev/worker_nodes.tf
+++ Infra/dev/worker_nodes.tf
@@ -1,37 +1,37 @@
-# module "worker_node_1" {
-#     source = "../modules/ec2/instance"
-#     ami = var.worker_node_ami
-#     instance_type = var.worker_node_instance_type
-#     subnet = module.subnet-2-private.id
-#     private_ip = var.worker_node_1_ip
-#     project = var.project
-#     env = var.env
-#     name = "worker-node-2-${var.project}-${var.env}"
-#     key_name = "${var.project}-${var.env}-${var.key_name}"
-#     security_groups = [module.worker_sg.id]
-#     user_data = var.worker_user_data
-# }
+module "worker_node_1" {
+    source = "../modules/ec2/instance"
+    ami = var.worker_node_ami
+    instance_type = var.worker_node_instance_type
+    subnet = module.subnet-2-private.id
+    private_ip = var.worker_node_1_ip
+    project = var.project
+    env = var.env
+    name = "worker-node-2-${var.project}-${var.env}"
+    key_name = "${var.project}-${var.env}-${var.key_name}"
+    security_groups = [module.worker_sg.id]
+    user_data = var.worker_user_data
+}
 
-# module "worker_node_2" {
-#     source = "../modules/ec2/instance"
-#     ami = var.worker_node_ami
-#     instance_type = var.worker_node_instance_type
-#     subnet = module.subnet-2-private.id
-#     private_ip = var.worker_node_2_ip
-#     project = var.project
-#     env = var.env
-#     name = "worker-node-2-${var.project}-${var.env}"
-#     key_name = "${var.project}-${var.env}-${var.key_name}"
-#     security_groups = [module.worker_sg.id]
-#     user_data = var.worker_user_data
+module "worker_node_2" {
+    source = "../modules/ec2/instance"
+    ami = var.worker_node_ami
+    instance_type = var.worker_node_instance_type
+    subnet = module.subnet-2-private.id
+    private_ip = var.worker_node_2_ip
+    project = var.project
+    env = var.env
+    name = "worker-node-2-${var.project}-${var.env}"
+    key_name = "${var.project}-${var.env}-${var.key_name}"
+    security_groups = [module.worker_sg.id]
+    user_data = var.worker_user_data
 
-# }
+}
 
-# module "worker_sg" {
-#     source = "../modules/network/security_groups"
-#     vpc_id = module.main-vpc.id
-#     ingress = var.worker_sg_ingress
-#     egress = var.worker_sg_egress
-#     project = var.project
-#     env = var.env
-# }
+module "worker_sg" {
+    source = "../modules/network/security_groups"
+    vpc_id = module.main-vpc.id
+    ingress = var.worker_sg_ingress
+    egress = var.worker_sg_egress
+    project = var.project
+    env = var.env
+}
